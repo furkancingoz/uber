07edaa994940d211f08e8fb2de4bd8ec3fbcfae1
Furkan Cingöz <furkancingoz@Mac.bbrouter>
2024-10-03 17:14:26 +0300

    create project

---
+++ models/driver.js
@@ -0,0 +1,13 @@
+class Driver {
+    constructor(name, location) {
+      this.name = name
+      this.location = location
+    }
+  
+    static create({name, location}) {
+      return new Driver(name, location)
+    }
+  }
+  
+  module.exports = Driver
+  
