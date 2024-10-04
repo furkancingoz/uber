97cae4dce052f030b6db61ae67b268a51191b670
Furkan Cingöz <furkancingoz@Mac.bbrouter>
2024-10-04 19:02:29 +0300

    Add passenger detail page and dynamic passenger listings in Pug
    - Created a new route `/passengers` to list all passengers with their names linking to individual detail pages.
    - Implemented a dynamic list of passengers on the homepage using Pug, where each passenger name is a clickable link that routes to their detail page.
    - Added a new route `/passengers/:passengerId` to display a passenger's name and booking history.
    - Handled scenarios where a passenger may not have any bookings by displaying "No booking" when the list is empty.
    - Set up Express to use Pug as the view engine for rendering dynamic HTML content.

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const flatted = require('flatted')
 
 const app = express()
 
+    app.set('view engine', 'pug')
+
 app.get('/passengers', async (req, res) => {
     const passengers = await passengerDatabase.load()
-    res.send(flatted.stringify(passengers))
+    //res.send(flatted.stringify(passengers))
+    res.render('passengers', { passengers })
+})
+
+app.get('/passengers/:passengerId', async (req, res) => {
+    const passenger = await passengerDatabase.find(req.params.passengerId)
+    if (!passenger) return res.status(404).send('Cannot find passengers')
+    res.render('passenger', { passenger })
 })
 
 app.get('/', (req, res) => {
-    res.send('homepage')
+    res.render('index')
 })
 
 app.listen(3000, () => {
